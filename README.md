dff4591330f1553146b01cdeb5561f4aa3f3b285
Ra <ranilsd86@gmail.com>
2023-08-17 21:56:36 -0400

    final push of my 7th or 8th try at a trivia game

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # scienceTriviaGame
-quiz game this was my 7th variation that i went through before i was able to get everything to work
+quiz game this was my 7th variation that i went through before i was able to get everything to work.
+it took ALOT!!! of research and help from friends, family, colleagues,  and tutors. there was alot of sacrifficed sleep
+
+this is a timed trivia game that tracks and stores high score by initials of the player.
+
+click to start game
+click answer and then submit in order to get to the next question
+once completed you will be prompted to enter and save your initials
+ click restart quize to start again
+ click vew high scores to access the high score list 
+ to clear the high scores click clear scores
---
+++ assets/questions.js
@@ -0,0 +1,92 @@
+
+//i created a second javascript for the questions because i kept on getting lost in code//
+// instructor said that any topic would be fine and that it did not have to be a coding quiz//
+const questions = [
+    {
+        text: "What bone in the human body does not connect to another bone?",
+        answers: [
+          'A) Hyoid Bone',
+          'B) Thigh Bone',
+          'C) Knee Bone',
+          'D) Funny Bone',
+        ],
+        correct: "A",
+      },
+
+      {
+        text: "Whome worked with Bill Gates to help find Microsoft?",
+        answers: [
+          'A) Benjamin Button',
+          'B) Willie Mays',
+          'C) Paul Allen',
+          'D) Andy Warhol',
+        ],
+        correct: "C",
+      },
+
+      {
+        text: "How many elements are in the periodic table?",
+        answers: [
+          'A) 130',
+          'B) 126',
+          'C) 115',
+          'D) 118',
+        ],
+        correct: "D",
+      },
+
+      {
+        text: "Dolly was the first living creature to be cloned. What type of animal was she?",
+        answers: [
+          'A) Dog',
+          'B) Sheep',
+          'C) Possum',
+          'D) Raccoon',
+        ],
+        correct: "B",
+      },
+
+      {
+        text: "Where are the smallest bones in the human body located?",
+        answers: [
+          'A) Wrist',
+          'B) Neck',
+          'C) Ear',
+          'D) Foot',
+        ],
+        correct: "C",
+      },
+
+      {
+        text: "What disease is named after the medieval term that means 'bad air'?",
+        answers: [
+          'A) Ebola',
+          'B) Polio',
+          'C) Parkinsons',
+          'D) Malaria',
+        ],
+        correct: "D",
+      },
+
+      {
+        text: "What is the largest internal organ of the human body?",
+        answers: [
+          'A) Stomach',
+          'B) Lung',
+          'C) Liver',
+          'D) Brain',
+        ],
+        correct: "C",
+      },
+
+      {
+        text: "What planet in our solar system has the most gravity?",
+        answers: [
+          'A) Jupiter',
+          'B) Uranus',
+          'C) Sun',
+          'D) Saturn',
+        ],
+        correct: "A",
+      },
+];
